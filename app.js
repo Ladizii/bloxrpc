9277b349d130f06fc5ba9b3bb04b9ca269f51a3c
Ladizii <sulphurous1983@gmail.com>
2020-08-02 17:00:32 -0500

    Code rework

--- app.js
+++ app.js
@@ -5,43 +5,17 @@ const nbx = require('noblox.js')
 async function run() {
     await nbx.setCookie(process.env.RBLXCookie);
     let interval = setInterval(async function() {
-    let presence = await nbx.getPresences([`${process.env.UserID}`])
-        if(presence.userPresences[0].userPresenceType === 0) {
-            client.updatePresence({
-                state: presence.userPresences[0].lastLocation,
-                details: "Offline",
-                largeImageKey: process.env.gameAssetID,
-                instance: true
-            })
-        }
-
-        if(presence.userPresences[0].userPresenceType === 1) {
-            client.updatePresence({
-                state: presence.userPresences[0].lastLocation,
-                details: "Online",
-                largeImageKey: process.env.gameAssetID,
-                instance: true
-            })
-        }
-
-        if(presence.userPresences[0].userPresenceType === 2) {
-            client.updatePresence({
-                state: presence.userPresences[0].lastLocation,
-                details: "In-Game",
-                largeImageKey: process.env.gameAssetID,
-                instance: true
-            })
-        }
-
-        if(presence.userPresences[0].userPresenceType === 3) {
-            client.updatePresence({
-                state: presence.userPresences[0].lastLocation,
-                details: "Studio",
-                largeImageKey: process.env.studioAssetID,
-                instance: true
-            })
-        }
-    }, 5000)
+    let presence = (await nbx.getPresences([process.env.UserID])).userPresences[0]
+    let details = ["Offline", "Online", "In-Game", "Studio"][presence.userPresenceType]
+    let large_image = presence.userPresenceType === 3 ? process.env.studioAssetID : process.env.gameAssetID
+    
+    client.updatePresence({
+      state: presence.lastLocation,
+      details: details,
+      largeImageKey: large_image,
+      instance: true
+    })
+}, 5000)
 }
  
 run();
